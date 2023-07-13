7b563e7f3da2cb2fd2a213deb24f87c47fe36a50
Sajid Khan <sajidk25@gmail.com>
2023-07-13 21:46:46 +0600

    1st commit to gitlab

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
-const caCertificatePath = '/src/config/X509-cert-5395350927322369437.pem';
+
 const connectMongo = () => {
   mongoose.set("strictQuery", true);
   mongoose
     .connect(`${process.env.MONGO_URI}`, {
       useNewURLParser: true,
       useUnifiedTopology: true,
-      sslCA: require('fs').readFileSync(caCertificatePath),
     })
     .then((conn) => {
       console.log(`Connected to: ${conn.connection.host}!`);
